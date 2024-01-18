45d174093fefb0cb620bf0ec8412dacaac8b28ae
Srs_vesta <101057653+SurajSanap@users.noreply.github.com>
2024-01-18 22:41:08 +0530

    Update 1_🏠Home.py
    removed the hide icon code

--- 1_🏠Home.py
+++ 1_🏠Home.py
@@ -4,19 +4,6 @@ import streamlit as st
 from streamlit_lottie import st_lottie 
 import json
 
-#Hide the icons
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 def home():
     st.set_page_config("College.ai",page_icon= '🏠', layout='centered')
