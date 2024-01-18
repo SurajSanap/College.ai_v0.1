de4bbb6669c5961e3c29c0d1e8428d580b5992ac
Srs_vesta <101057653+SurajSanap@users.noreply.github.com>
2024-01-18 23:18:34 +0530

    Update 10_ℹ️About.py

--- pages/10_ℹ️About.py
+++ pages/10_ℹ️About.py
@@ -30,7 +30,7 @@ def ResumeAnalyzer():
 
 def show_thank_you_emoji():
     # Display 10 heart emojis
-    hearts = " ❤️" * 10
+    hearts = " ❤️" * 3
     st.markdown(f"{hearts}")
     # Wait for 2 seconds
     time.sleep(2)
