fa1a184a2ca9486bb7f3dffe46dd251b80f77f2c
Srs_vesta <101057653+SurajSanap@users.noreply.github.com>
2024-01-18 23:43:58 +0530

    Update 10_ℹ️About.py

--- pages/10_ℹ️About.py
+++ pages/10_ℹ️About.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_lottie import st_lottie 
 import json
 
-st.title(" - - - - - - - -  About  - - - - - - - - -")
+st.title("About us")
 
 st.header("Page:")
 def Ask_To_PDF():
